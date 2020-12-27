0b8ff459c3fd24e2d211e1530de252a7d906b920
Ultraschemer <paulosfilho@gmail.com>
2020-12-26 23:37:44 -0300

    Implemented testbed. Webpack configuration and Javascript/Typescript integration files added.

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { testProc, variable } from './testbed'
+document.write('Starting something here - variable: ', variable)
+testProc()
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+
+module.exports = (env) => {
+  const isProduction = env === 'production'
+  return {
+    entry: './src/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules|server/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    output: {
+      filename: 'public/javascript/bundle.js',
+      path: path.resolve(__dirname),
+    },
+    resolve: {
+      modules: ['node_modules', 'src'],
+    },
+    optimization: {
+      minimize: true,
+      mergeDuplicateChunks: false,
+    },
+    devtool: isProduction ? 'none' : 'inline-source-map',
+    devtool: 'none'
+  }
+}
